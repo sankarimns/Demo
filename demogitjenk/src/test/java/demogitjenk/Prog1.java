10e428c68aba75c9274144251cfbbf172414de23
ssankarasubbu <ssankarasubbu@lm-maa-00668873.paypalcorp.com>
2017-10-19 05:11:20 -0700

    first commit

---
+++ demogitjenk/src/test/java/demogitjenk/Prog1.java
@@ -0,0 +1,14 @@
+package demogitjenk;
+
+import org.testng.annotations.Test;
+
+@Test(priority = 1)
+public class Prog1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("HelloWorld");
+
+	}
+
+}
---
+++ demogitjenk/src/test/java/demogitjenk/Prog2.java
@@ -0,0 +1,10 @@
+package demogitjenk;
+
+import org.testng.annotations.Test;
+
+@Test(priority = 2)
+public class Prog2 {
+public void jen() {
+	System.out.println("helloworld2");
+}
+}
